0beb2e4df4eaa42165d143463e762cc87ca6ec35
cleverson <cleverson.s.dias@gmail.com>
2019-03-19 15:06:41 -0300

    My Money App

--- my-money-app/backend/src/config/routes.js
+++ my-money-app/backend/src/config/routes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 
-module.exports = function(server) {
+module.exports = server => {
 	// Definir URL base para todas as rotas
 	const router = express.Router()
 	server.use('/api', router)

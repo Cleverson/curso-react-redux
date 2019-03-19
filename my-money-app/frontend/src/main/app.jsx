6358d07860ede1f17e16e9dce7b0f023ca2835b3
cleverson <cleverson.s.dias@gmail.com>
2019-03-19 11:44:09 -0300

    My Money App

---
+++ my-money-app/frontend/src/main/app.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../common/template/dependencies'
+
+import Hearder from '../common/template/header'
+
+const App = () => {
+	return (
+		<div className="wrapper">
+			<Hearder />
+		</div>
+	)
+}
+
+export default App

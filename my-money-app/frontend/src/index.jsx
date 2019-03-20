4f61f62f9195fd2aaa5ec3106dfc9a7f387f2628
cleverson <cleverson.s.dias@gmail.com>
2019-03-20 14:44:18 -0300

    My Money App

--- my-money-app/frontend/src/index.jsx
+++ my-money-app/frontend/src/index.jsx
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom'
 
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
+import reducers from './main/reducers'
 
 import App from './main/app'
-import reducers from './main/reducers'
 
 const store = createStore(reducers)
 
